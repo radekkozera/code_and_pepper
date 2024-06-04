2388185e05a9b7a867ed407c707cc747d66455a8
Radek Kozera <radoslaw.kozera@bytamic.com>
2024-06-04 15:33:43 +0200

    feat: angular project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgParticlesService } from '@tsparticles/angular';
+import { loadSlim } from '@tsparticles/slim';
+import { particlesOptions } from './helpers/particles';
+import { ISourceOptions } from '@tsparticles/engine';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'code_and_pepper';
+
+  constructor(private _particlesService: NgParticlesService) { }
+
+  particlesOptions: any = particlesOptions;
+
+  ngOnInit(): void {
+    this._loadParticles();
+  }
+
+
+  private _loadParticles(): void {
+    this._particlesService.init(async (engine) => {
+      await loadSlim(engine)
+    })
+  }
 }
